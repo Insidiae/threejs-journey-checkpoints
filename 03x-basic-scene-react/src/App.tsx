5fd397b0710c2a66a5b18708ee90bcaa8543cff7
Kobe Ruado <insidiae423@gmail.com>
2023-05-26 01:01:22 +0800

    Recreate Lesson 3 in R3F

---
+++ 03x-basic-scene-react/src/App.tsx
@@ -0,0 +1,48 @@
+import * as React from "react";
+import { useThree, Canvas } from "@react-three/fiber";
+
+const sizes = {
+	width: 800,
+	height: 600,
+};
+
+function Box() {
+	const state = useThree();
+
+	React.useEffect(() => {
+		state.setSize(sizes.width, sizes.height);
+		state.camera.position.set(1, 2, 3);
+	}, [state]);
+
+	return (
+		<mesh>
+			<boxGeometry args={[1, 1, 1]} />
+			<meshBasicMaterial color={0x588dfd} />
+		</mesh>
+	);
+}
+
+function App() {
+	return (
+		<div
+			id="canvas-container"
+			//? Just apply the same black background from the vanilla example
+			style={{
+				...sizes,
+				background: "black",
+			}}
+		>
+			<Canvas
+				camera={{
+					fov: 75,
+					aspect: sizes.width / sizes.height,
+					// position: [1, 2, 3],
+				}}
+			>
+				<Box />
+			</Canvas>
+		</div>
+	);
+}
+
+export default App;
